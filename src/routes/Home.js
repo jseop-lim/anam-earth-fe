b3976bc5df979aecb59a368adc617aa9b3857080
YHP <dkwjt001@naver.com>
2022-08-27 22:40:55 +0900

    button css추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Writing from '../components/Writing';
 import '../App.css';
-
+import '../css/button.css';
 const SERVER_URL = 'http://anam-earth-api.jseoplim.com/board/posts';
 
 function Home() {
