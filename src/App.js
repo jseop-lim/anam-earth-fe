76c1b4828d9d4d47a25d1b731c17af87c720148a
Yoon Hwa Pyeong <dkwjt001@korea.ac.kr>
2022-08-28 12:01:52 +0900

    Fix: solve underscore problem

--- src/App.js
+++ src/App.js
@@ -1,89 +1,88 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import 'antd/dist/antd.css';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "antd/dist/antd.css";
 
-import Barrier_free from './routes/Barrier_free';
+import BarrierFree from "./routes/BarrierFree";
 
-import CustomLayout from './containers/Layout';
-import BoardPage from './components/BoardPage';
-import Search_result from './routes/Search_result';
+import CustomLayout from "./containers/Layout";
+import BoardPage from "./components/BoardPage";
+import SearchResult from "./routes/SearchResult";
 
 // 게시판 components
-import TextEditer from './routes/TextEditer';
-import Home from './routes/Home';
-import ViewDetail from './routes/ViewDetail';
+import TextEditer from "./routes/TextEditer";
+import Home from "./routes/Home";
+import ViewDetail from "./routes/ViewDetail";
 
 // 카카오 지도
-import Map from './routes/PathSearch';
+import Map from "./routes/PathSearch";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route
-                    exact
-                    path="/Barrier_free"
-                    element={
-                        <CustomLayout>
-                            <Barrier_free />
-                        </CustomLayout>
-                    }
-                />
-                <Route
-                    exact
-                    path="/"
-                    element={
-                        <CustomLayout>
-                            <BoardPage />
-                        </CustomLayout>
-                    }
-                />
-                <Route
-                    exact
-                    path="anam/:s_lat/:s_lng/:e_lat/:e_lng"
-                    element={
-                        <CustomLayout>
-                            <Search_result />
-                        </CustomLayout>
-                    }
-                />
-                <Route
-                    exact
-                    path="kakaomap"
-                    element={
-                        <CustomLayout>
-                            <Map />
-                        </CustomLayout>
-                    }
-                />
-                <Route
-                    path="editing"
-                    element={
-                        <CustomLayout>
-                            <TextEditer />
-                        </CustomLayout>
-                    }
-                />
-                <Route
-                    path="view"
-                    element={
-                        <CustomLayout>
-                            <Home />
-                        </CustomLayout>
-                    }
-                />
-                <Route
-                    path="view/:id"
-                    element={
-                        <CustomLayout>
-                            <ViewDetail />
-                        </CustomLayout>
-                    }
-                />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route
+          exact
+          path="/Barrier_free"
+          element={
+            <CustomLayout>
+              <BarrierFree />
+            </CustomLayout>
+          }
+        />
+        <Route
+          exact
+          path="/"
+          element={
+            <CustomLayout>
+              <BoardPage />
+            </CustomLayout>
+          }
+        />
+        <Route
+          exact
+          path="anam/:s_lat/:s_lng/:e_lat/:e_lng"
+          element={
+            <CustomLayout>
+              <SearchResult />
+            </CustomLayout>
+          }
+        />
+        <Route
+          exact
+          path="kakaomap"
+          element={
+            <CustomLayout>
+              <Map />
+            </CustomLayout>
+          }
+        />
+        <Route
+          path="editing"
+          element={
+            <CustomLayout>
+              <TextEditer />
+            </CustomLayout>
+          }
+        />
+        <Route
+          path="view"
+          element={
+            <CustomLayout>
+              <Home />
+            </CustomLayout>
+          }
+        />
+        <Route
+          path="view/:id"
+          element={
+            <CustomLayout>
+              <ViewDetail />
+            </CustomLayout>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
