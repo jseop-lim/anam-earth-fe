2a2feb052761a4abf48ee3da3444c019eddd08af
jseop-lim <jseoplim@gmail.com>
2022-09-17 16:07:04 +0900

    Deploy: 배포 스크립트 수정

--- deploy/after-install.sh
+++ deploy/after-install.sh
@@ -2,5 +2,8 @@
 
 cd ~/anam-earth/deploy
 
-# Stop and remove containers
-docker compose down
+if [ "$(docker ps -aq -f name=nginx)" ]; then
+    docker compose start nginx
+else
+    docker compose up -d
+fi  
